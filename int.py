6f046b9613ea0a5a21e3b47c80a871de19987485
njugushh <92704534+njugushh@users.noreply.github.com>
2024-08-14 22:53:18 +0300

    Int

---
+++ int.py
@@ -0,0 +1,16 @@
+# Nest functions
+# [] - optional items inside
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+z = round(x + y)
+print(f"{z:,}")
+
+# FLOAT
+
+v = float(input("What's v? "))
+w = float(input("What's w? "))
+
+h = v / w
+print(f"{h:.2f}") 
