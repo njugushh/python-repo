c9be409f55f3bad7b1de60c16c4ab7201fe9ac81
njugushh <92704534+njugushh@users.noreply.github.com>
2024-08-15 20:57:15 +0300

    Condition

--- condition.py
+++ condition.py
@@ -3,13 +3,7 @@
 x = int(input("What is x? "))
 y = int(input("What is y? "))
 
-if x < y:
-    print("x is less than y")
-    
-elif x > y:
-    print("x is greater than y")
-    
-elif x == y:
+if x == y:
     print("x is equal to y")
     
 else :
